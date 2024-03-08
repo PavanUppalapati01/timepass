aa887fcdbebc726a4bd662a69652c906e54f6aef
Uppalapati sai ganesh pavan varma <96948122+PavanUppalapati01@users.noreply.github.com>
2024-03-08 18:04:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    value = int(request.form['value'])
+    if 1 <= value <= 100:
+        return render_template('welcome.html')
+    else:
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
